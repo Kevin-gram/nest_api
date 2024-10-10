012dd2e0039b0e0793d8cf0a624cbca2e8d83f45
Kevin-gram <k.nyiringan@alustudent.com>
2024-10-10 13:03:57 +0200

    creating an interface for items

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
-
+import { Item } from './interface/item.interface';
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private readonly items: Item[] = [
+    {
+      id: 24324234,
+      name: 'peter',
+      description: 'kanyarwanda',
+      qty: 5,
+    },
+  ];
+}
